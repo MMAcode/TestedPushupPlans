d7b10fd411028ba6bd6af3fc5f2f6d7143637f55
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-08-12 11:14:52 +0100

    Updates

---
+++ service-worker.js
@@ -0,0 +1,77 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MERN-ReactForExpress/precache-manifest.fb1de1851d07da120086a44789276260.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MERN-ReactForExpress/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  console.log('New notification', data);
+  const options = {
+    body: data.body,
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+})
+
+// self.addEventListener('install', e => {
+//   console.log("MM c-sw install event fired");
+// })
+
+
+// self.addEventListener('notificationclose', function (e) {
+//   var notification = e.notification;
+//   var data = notification.data || {};
+//   var primaryKey = data.primaryKey;
+//   console.log('Closed notification: ' + primaryKey);
+// });
+// self.addEventListener('notificationclick', function (e) {
+//   var notification = e.notification;
+//   var data = notification.data || {};
+//   var primaryKey = data.primaryKey;
+//   var action = e.action;
+//   console.log('Clicked notification: ' + primaryKey);
+//   if (action === 'close') {
+//     console.log('Notification clicked and closed', primaryKey);
+//     notification.close();
+//   }
+//   else {
+//     console.log('Notification actioned', primaryKey);
+//     clients.openWindow('/');
+//     notification.close();
+//   }
+// });
